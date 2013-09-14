2b6da9d9e83bd112cc4772fde72f26bd1bfb9395
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2013-09-14 18:57:37 -0500

    basic main.html and login page

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
+    url(r'^$', login),
     # url(r'^app/', include('app.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -24,5 +24,4 @@ urlpatterns = patterns('',
 
     url(r'^meow/add', add_meow),
     url(r'^user/subscribe/(\d+)', subscribe_user)
-    
 )
