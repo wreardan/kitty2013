66cfd31488ae18d3bdfa89e1e8d959230321b61f
Wesley Reardan <reardan@gmail.com>
2013-09-18 20:12:08 -0500

    Added image loading form.

--- app/settings.py
+++ app/settings.py
@@ -17,11 +17,6 @@ DATABASES = {
     'local': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kitty2013.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
     },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
