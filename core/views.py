6c294cfb75a9e520f40454c02ce763b9b896754a
Wesley Reardan <reardan@gmail.com>
2013-09-19 20:31:56 -0500

    Got friend relationship working.

--- core/views.py
+++ core/views.py
@@ -107,6 +107,7 @@ def user_home(request, user_id):
     followers = user.userprofile.followers.all()
     following = user.userprofile.userprofile_set.all()
 
+    #Bob Edit 8:13pm
     suggestions = UserProfile.objects.all()
 
     if logged_user == user:   
